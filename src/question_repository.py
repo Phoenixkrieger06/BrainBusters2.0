527041927cb039c2951252c660cf97d517d533a3
Phoenixkrieger06 <wyatt.loevenich@gmail.com>
2026-03-27 14:27:02 +0100

    Update question_repository.py

--- src/question_repository.py
+++ src/question_repository.py
@@ -1,1 +1,9 @@
-#Placeholder
+import json
+
+class QuestionRepository:
+    @staticmethod
+    def load_questions(category):
+        with open("data/questions.json", "r", encoding="utf-8") as f:
+            all_questions = json.load(f)
+
+        return [q for q in all_questions if q["category"] == category]
